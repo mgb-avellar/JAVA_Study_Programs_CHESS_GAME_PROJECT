2d8aa7bcc2400959aba877d3d945646eddf74865
Marcio Guilherme Bronzato de Avellar <mgb.avellar@gmail.com>
2021-02-23 18:16:57 -0300

    first class created: Position

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
+import boardgame.Position;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Kick-off of the project.");
+        System.out.println("Printing position (testing:)");
+
+        Position position = new Position(3 ,4);
+
+        System.out.println(position);
     }
 }
