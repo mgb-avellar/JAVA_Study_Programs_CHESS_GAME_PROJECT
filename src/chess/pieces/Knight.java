de4c26680f1c475a3d42e2412eb8e7f6a8cbf0d0
Marcio Guilherme Bronzato de Avellar <mgb.avellar@gmail.com>
2021-02-26 16:45:56 -0300

    implementing Knight

--- src/chess/pieces/Knight.java
+++ src/chess/pieces/Knight.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -22,13 +23,66 @@ public class Knight extends ChessPiece {
          */
     }
 
+    /*
+    The Knight movements are different from the other pieces: it can move two squares ahead or behind and
+    one to the left or right subsequently. This movement has a L-shape.
+    To implement the possible moves of a Knight, we need to know if it can move to the next square
+    in this peculiar fashion each time.
+    The codification, though, is similar to the one of the King, with 8 possibilities.
+     */
+
+    private boolean canMove(Position position) {
+        ChessPiece p = (ChessPiece)getBoard().piece(position);
+        return p == null || p.getColor() != getColor();
+    }
+
     @Override
     public boolean[][] possibleMoves() {
-        /*
-         For now, we are not going to implement the possible moves of the Knight.
-         We are just going to create a boolean matrix of the size of the board.
-         */
+
         boolean[][] matrix = new boolean[getBoard().getNumberRows()][getBoard().getNumberColumns()];
+
+        Position p = new Position(0, 0);
+
+        p.setValues(position.getRow() - 1, position.getColumn() - 2);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            matrix[p.getRow()][p.getColumn()] = true;
+        }
+
+        p.setValues(position.getRow() - 2, position.getColumn() - 1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            matrix[p.getRow()][p.getColumn()] = true;
+        }
+
+        p.setValues(position.getRow() - 2, position.getColumn() + 1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            matrix[p.getRow()][p.getColumn()] = true;
+        }
+
+        p.setValues(position.getRow() - 1, position.getColumn() + 2);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            matrix[p.getRow()][p.getColumn()] = true;
+        }
+
+        p.setValues(position.getRow() + 1, position.getColumn() + 2);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            matrix[p.getRow()][p.getColumn()] = true;
+        }
+
+        p.setValues(position.getRow() + 2, position.getColumn() + 1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            matrix[p.getRow()][p.getColumn()] = true;
+        }
+
+        p.setValues(position.getRow() + 2, position.getColumn() - 1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            matrix[p.getRow()][p.getColumn()] = true;
+        }
+
+        p.setValues(position.getRow() + 1, position.getColumn() - 2);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            matrix[p.getRow()][p.getColumn()] = true;
+        }
+
         return matrix;
     }
 }
